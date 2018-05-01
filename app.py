62af8fae5596443172825754bdf4b89bd1eaff36
Sebastian Stoll <seb_stoll@gmx.de>
2018-05-01 20:14:42 +0200

    added app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+from sklearn.externals import joblib
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
+            age = float(data['age'])
+
+            lin_reg = joblib.load('linear_regression_model.pkl')
+        except ValueError:
+            return jsonify('Please enter a number.')
+
+        return jsonify(lin_reg.predict(age).tolist())
+
+if __name__ == '__main__':
+    app.run(debug=True)
